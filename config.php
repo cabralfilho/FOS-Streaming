c62e8cf78c04e57f653c3e50d9e5050df8fe30ab
cabral-filho <cabralfilhodj@gmail.com>
2019-01-08 00:41:35 -0200

    Update config.php

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ include('_start.php');
 $databasemanagar->addConnection([
     'driver'    => 'mysql',
     'host'      => 'xxxx',
-    'database'  => 'xxxx',
-    'username'  => 'xxxx',
-    'password'  => 'xxxx',
+    'database'  => 'wwww',
+    'username'  => 'kkkk',
+    'password'  => 'yyyy',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
